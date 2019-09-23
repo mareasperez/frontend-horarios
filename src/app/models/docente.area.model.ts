a285c69f2ccaa53128112fae793093872edc7be5
maury <mareasperez7@gmail.com>
2019-09-23 05:32:36 -0600

    cargas completo

--- src/app/models/docente.area.model.ts
+++ src/app/models/docente.area.model.ts
@@ -1,6 +1,6 @@
 
 export class DocenteAreaModel{
-    public da_id: string;
+    public da_id: number;
     public da_area: string;
     public da_docente: string;
 
--- src/app/models/rep.carga.ts
+++ src/app/models/rep.carga.ts
@@ -1,4 +1,5 @@
 import {RComponent} from '../models/rcomponentes';
+import { DocenteHorasModel } from './docente.horas.model';
 export class ReporteCargaModel {
 docente: string;
 componente: RComponent[];
