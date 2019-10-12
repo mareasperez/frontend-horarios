b90c0eb0b37fa72e1463fbeaf508332613c896df
maury <mareasperez7@gmail.com>
2019-10-12 00:00:55 -0600

    add 2 pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,6 +61,8 @@ import { HorariosAnyoComponent } from './components/reportes/horarios-anyo/horar
 import { getNombreFacultadPipe } from './components/departamento/verdepartamento/get-nombre-facultad.pipe';
 import { DocHorasComponent } from './components/doc-horas/doc-horas.component';
 import { DocHorasAddComponent } from './components/doc-horas-add/doc-horas-add.component';
+import { FacultadesPipe } from './pipes/facultades.pipe';
+import { DepartamentoNamePipe } from './pipes/departamento-name.pipe';
 
 @NgModule({
   declarations: [
@@ -96,6 +98,8 @@ import { DocHorasAddComponent } from './components/doc-horas-add/doc-horas-add.c
     getNombreFacultadPipe,
     DocHorasComponent,
     DocHorasAddComponent,
+    FacultadesPipe,
+    DepartamentoNamePipe,
   ], entryComponents: [
     AddPlanificacionComponent,
     DocHorasAddComponent,
