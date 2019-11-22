7d5628efc31beffbbf9123d244559131e69760e8
maury <mareasperez7@gmail.com>
2019-11-22 00:53:12 -0600

    add carrera type

--- src/app/models/carrera.model.ts
+++ src/app/models/carrera.model.ts
@@ -2,5 +2,6 @@ export class CarreraModel {
         // tslint:disable: variable-name
         public carrera_id: string;
         public carrera_nombre: string;
+        public carrera_tipo: string;
         public carrera_departamento: string;
 }
