0e2d778486b47c2f87d9e18a41a6306c5f980384
maury <mareasperez7@gmail.com>
2019-11-22 02:11:38 -0600

    aula edit dialog fix + model changed

--- src/app/models/aula.model.ts
+++ src/app/models/aula.model.ts
@@ -1,10 +1,8 @@
-
-
-export class AulaModel{
+export class AulaModel {
     // tslint:disable: variable-name
     public aula_id: string;
     public aula_nombre: string;
     public aula_capacidad: number;
-    public aula_tipo: string;
+    public aula_tipo: boolean;
     public aula_recinto: string;
 }
