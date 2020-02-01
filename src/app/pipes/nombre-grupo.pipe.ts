0c05161e1fffe888996b2a97f1e6e6849aad755f
Kenneth Somarriba <kennethsomarriba96@gmail.com>
2020-02-01 11:28:46 -0600

    revision pipes para reportes de horarios

--- src/app/pipes/nombre-grupo.pipe.ts
+++ src/app/pipes/nombre-grupo.pipe.ts
@@ -7,12 +7,17 @@ import { GrupoModel } from '../models/grupo.model';
 export class NombreGrupoPipe implements PipeTransform {
 
   transform(id: string, grupos: GrupoModel[]): any {
-    if (id !== undefined && grupos.length<0) {
-      console.log('se recibe  id: ', id, 'grupos:', grupos);
-      const grupo = grupos.find(g => id === g.grupo_id);
-      return grupo.grupo_tipo + grupo.grupo_numero;
+    if (id !== undefined && grupos.length > 0) {
+      if ( id !== null) {
+        console.log('se recibe  id: ', id, 'grupos:', grupos);
+        const grupo = grupos.find(g => id === g.grupo_id);
+        return grupo.grupo_tipo + grupo.grupo_numero;
+      } else {
+        return 'Sin Asignar Grupo';
+      }
+    } else {
+      return 'Sin Asignar Grupo';
     }
-    return '';
   }
 
 }
