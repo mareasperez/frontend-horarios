062d0101b325dca2e7ce1f9d1bc6329b27f2cf98
Kenneth Somarriba <kennethsomarriba96@gmail.com>
2020-08-29 00:17:46 -0600

    solo cambios visuales, en reportes + adicion de iconos y un fondo

---
+++ src/app/pipes/gp-docente.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DocenteModel } from '../models/docente.model';
+import { GrupoModel } from '../models/grupo.model';
+
+@Pipe({
+  name: 'gpDocente'
+})
+export class GpDocentePipe implements PipeTransform {
+
+  transform(id: string, docentes: DocenteModel[], grupos: GrupoModel[]): DocenteModel {
+    if (id !== undefined && id !== null && docentes.length > 0 && grupos.length > 0) {
+      const grupo: GrupoModel = grupos.find(gp => id === gp.grupo_id);
+      const docente: DocenteModel = docentes.find((a: DocenteModel) => grupo.grupo_docente === a.docente_id);
+      return docente;
+    }
+    else {
+      console.log('error de busqueda de Docente con id: ' + id);
+      const docente = new DocenteModel();
+      docente.docente_nombre = 'busqueda fallida';
+      return docente;
+    }
+  }
+}
