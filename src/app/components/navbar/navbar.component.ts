ca690968af762400f986e5809b5c990afb3bc3b2
Jorge Navarrete <jorge1793.navarrete@gmail.com>
2020-11-11 00:45:43 -0600

    limpiar local storage al cerrar secion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { JwtService } from '../../services/jwt.service';
-import { variable } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from "@angular/core";
+import { JwtService } from "../../services/jwt.service";
+import { variable } from "@angular/compiler/src/output/output_ast";
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
-
   // tslint:disable-next-line: no-shadowed-variable
-  constructor(private JwtService: JwtService) { }
+  constructor(private JwtService: JwtService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   logout() {
+    localStorage.clear();
     this.JwtService.logout();
   }
 
